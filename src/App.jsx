45859ba5bf4e91a6be979c466a27f7d58b139adc
you823 <youssef.ashraf.eng@gmail.com>
2025-04-22 20:51:27 +0200

    add gh-pages config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter  as Router } from 'react-router-dom';
 import AppRoutes from "./routes/index.jsx";
 
 function App() {
 
   return (
-    <Router>
+    <Router basename="/">
       <AppRoutes />
     </Router>
   )
