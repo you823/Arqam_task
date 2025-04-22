ce004951d6c9e27ce5423b7f672394ba67a18559
you823 <youssef.ashraf.eng@gmail.com>
2025-04-22 21:35:04 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://yourusername.github.io/your-repo-name/', // Add this for GitHub Pages
   build: {
     outDir: 'dist', // Vite's default build directory
   }
